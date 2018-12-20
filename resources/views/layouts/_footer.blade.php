d75646dded6cf726c7161caf2760ed13fc318188
ixingjue <826739558@qq.com>
2018-12-20 12:50:49 +0800

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由
+            <a href="https://github.com/ixingjue/larabbs" target="_blank">无招</a>
+            设计和编码
+            <span style="color: #e27575;font-size: 14px">❤</span>
+        </p>
+        <p class="pull-right">
+            <a href="mailto:name@email.com">联系我们</a>
+        </p>
+    </div>
+</footer>
