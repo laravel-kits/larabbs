7d6db6c37c0497b69fb699348014e57222033239
ixingjue <826739558@qq.com>
2018-12-27 23:09:54 +0800

    Add queue for translate slug

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handles\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -17,9 +18,16 @@ class TopicObserver
         $topic->body = clean($topic->body, 'user_topic_body');
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
-        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
+    }
+
+    // 模型监控器的 saved() 方法对应 Eloquent 的 saved 事件，此事件发生在创建和编辑时、数据入库以后。在 saved() 方法中调用，
+    // 确保了我们在分发任务时，$topic->id 永远有值。
+    public function saved(Topic $topic)
+    {
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译 isDirty判断是都是更新数据
         if (!$topic->slug || $topic->isDirty()) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
             // 修复edit或者编辑的时候会跑到路由后面的问题
             // @url https://laravel-china.org/topics/14584/slug-has-bug?#reply76507
             if (trim($topic->slug) === 'edit') {
