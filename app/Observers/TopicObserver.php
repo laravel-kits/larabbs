27adf0d9f8020340fde79fc405db357ff098c6cd
ixingjue <826739558@qq.com>
2018-12-27 22:30:37 +0800

    修改标题更新slug`https://laravel-china.org/topics/16776/modify-the-slug-content-after-the-title`

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -18,7 +18,7 @@ class TopicObserver
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
-        if (!$topic->slug) {
+        if (!$topic->slug || $topic->isDirty()) {
             $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
             // 修复edit或者编辑的时候会跑到路由后面的问题
             // @url https://laravel-china.org/topics/14584/slug-has-bug?#reply76507
