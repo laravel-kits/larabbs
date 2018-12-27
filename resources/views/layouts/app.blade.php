475fa9d319b55f14da5917e95dca60a10dc1ad81
ixingjue <826739558@qq.com>
2018-12-27 13:33:49 +0800

    WYSIWYG Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
 </head>
 <body>
 <div id="app" class="{{route_class()}}-page">
@@ -22,5 +23,6 @@
 </div>
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}"></script>
+@yield('scripts')
 </body>
 </html>
