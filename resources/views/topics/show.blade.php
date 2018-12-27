4e260743f96cec34a9bf0d374d13edbc0a575e09
ixingjue <826739558@qq.com>
2018-12-27 18:34:37 +0800

    Author can delete topic

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -36,15 +36,17 @@
                     <div class="topic-body">
                         {!! $topic->body !!}
                     </div>
-                    <div class="operate">
-                        <hr>
-                        <a href="{{route('topics.edit',$topic->id)}}" class="btn btn-default btn-xs" role="button">
-                            <i class="glyphicon glyphicon-edit"></i> 编辑
-                        </a>
-                        <a href="#" class="btn btn-default btn-xs" role="button">
-                            <i class="glyphicon glyphicon-trash"></i> 删除
-                        </a>
-                    </div>
+                    @can('update',$topic)
+                        <div class="operate">
+                            <hr>
+                            <a href="{{route('topics.edit',$topic->id)}}" class="btn btn-default btn-xs" role="button">
+                                <i class="glyphicon glyphicon-edit"></i> 编辑
+                            </a>
+                            <a href="#" class="btn btn-default btn-xs" role="button">
+                                <i class="glyphicon glyphicon-trash"></i> 删除
+                            </a>
+                        </div>
+                    @endcan
                 </div>
             </div>
         </div>
