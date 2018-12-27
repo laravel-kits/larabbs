db091af72a0a080c5baff883b791632aa22c1461
ixingjue <826739558@qq.com>
2018-12-27 20:54:59 +0800

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::group(['middleware' => 'auth'], function () {
     });
     // 结束
 });
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topics/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
