314e46e6fdbaff44191b45a867d915201beb9089
ixingjue <826739558@qq.com>
2018-12-28 12:41:39 +0800

    fixed bug

--- resources/views/users/_topics.blade.php
+++ resources/views/users/_topics.blade.php
@@ -2,7 +2,7 @@
     <ul class="list-group">
         @foreach ($topics as $topic)
             <li class="list-group-item">
-                <a href="{{$topic->links()}}">
+                <a href="{{$topic->link()}}">
                     {{$topic->title}}
                 </a>
                 <span class="meta pull-right">
