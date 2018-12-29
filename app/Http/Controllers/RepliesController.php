d4091b7be67c26c3a61fe81864d386b63c5bc920
ixingjue <826739558@qq.com>
2018-12-29 19:39:30 +0800

    改成input方法

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -17,7 +17,7 @@ class RepliesController extends Controller
     public function store(ReplyRequest $request, Reply $reply)
     {
         //@url https://laravel-china.org/topics/8731/when-the-content-is-empty-when-the-content-is-filtered-use-a-more-friendly-way-to-handle-the-xss
-        $content = clean($request->post('content'), 'user_topic_body');
+        $content = clean($request->input('content'), 'user_topic_body'); // 避免与 Request 对象本身的 public 属性冲突，比如 attributes / query 可以很方便地使用 input() 的第二个参数来返回默认值
         if (empty($content)) {
             return redirect()->back()->with('danger', '回复内容错误！');
         }
