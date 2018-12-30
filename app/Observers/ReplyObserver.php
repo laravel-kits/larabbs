c32e0d7a17a8feb257d84923ff44e4254f111ffa
ixingjue <826739558@qq.com>
2018-12-30 11:07:05 +0800

    User can delete reply

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -21,4 +21,9 @@ class ReplyObserver
         // 通知作者话题被回复了
         $topic->user->topicNotify(new TopicReplied($reply));
     }
+
+    public function deleted(Reply $reply)
+    {
+        $reply->topic->decrement('reply_count', 1);
+    }
 }
