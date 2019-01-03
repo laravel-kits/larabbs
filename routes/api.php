1af7d8f4e3fad60e4fc2fd5d2ce46904ab9c66f8
ixingjue <826739558@qq.com>
2019-01-03 22:17:50 +0800

    rate limit

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,20 @@ use Illuminate\Http\Request;
 */
 
 $api = app('Dingo\Api\Routing\Router');
+
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api',
 ], function ($api) {
-    // 短信验证码
-    $api->post('verificationCodes', 'VerificationCodesController@store')
-        ->name('api.verificationCodes.store');
-    // 用户注册
-    $api->post('users', 'UsersController@store')
-        ->name('api.users.store');
+    $api->group([
+        'middleware' => 'api.throttle',
+        'limit' => config('api.rate_limits.sign.limit'),
+        'expires' => config('api.rate_limits.access.expires'),
+    ], function ($api) {
+        // 短信验证码
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
+        // 用户注册
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
+    });
 });
