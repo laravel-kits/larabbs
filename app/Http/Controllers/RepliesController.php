a56a91c631f7ec8854aeff6ea411197f329be5b8
ixingjue <826739558@qq.com>
2019-01-09 16:46:25 +0800

    重构@某人逻辑
    (cherry picked from commit ff6055d4f4caf1d52e46c73e019a437bb027c4c4)

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -22,11 +22,6 @@ class RepliesController extends Controller
         if (empty($content)) {
             return redirect()->back()->with('danger', '回复内容错误！');
         }
-        // fixme只能@一个用户
-        $username = $this->get_between($content, '@', ' ');
-        $uid = User::query()->where('name', $username)->pluck('id')->toArray();
-        $replace = "<a style='color:blue' href='/users/" . $uid[0] . "' title='" . "$username'>@" . $username . "</a>";
-        $content = str_replace('@' . $username, $replace, $content);
         $reply->content = $content;
         $reply->user_id = \Auth::id();
         $reply->topic_id = $request->topic_id;
@@ -50,18 +45,5 @@ class RepliesController extends Controller
         $users = User::query()->where('name', 'like', $name . '%')->pluck('name');
         // laravel集合可以当做数组用，laravel默认返回的是json响应
         return $users;
-    }
-
-    /**
-     * php截取指定两个字符之间字符串
-     * @param $input
-     * @param $start
-     * @param $end
-     * @return bool|string
-     */
-    public function get_between($input, $start, $end)
-    {
-        $substr = substr($input, strlen($start) + strpos($input, $start), (strlen($input) - strpos($input, $end)) * (-1));
-        return $substr;
     }
 }
